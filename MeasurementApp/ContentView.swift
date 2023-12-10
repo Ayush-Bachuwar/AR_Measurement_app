a8c6111bd1edd9d494afbcea1f641a79f9780a9e
Ayush-Bachuwar <76203763+Ayush-Bachuwar@users.noreply.github.com>
2023-12-10 13:37:29 +0530

    Add files via upload

---
+++ MeasurementApp/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  MeasurementApp
+//
+//  Created by ayush bachuwar on 7/12/23.
+
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        arView.session.run(config)
+        
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap)))
+        context.coordinator.arView = arView
+        context.coordinator.setupUI()
+        
+        arView.addCoachingOverlay()
+        
+        return arView
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
